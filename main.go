ee98192f7d4054074f2f8d10d591b0706a8b4a7e
httpjamesm <github@httpjames.space>
2023-05-22 13:51:23 -0400

    feat: launchpad homepage

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	app := gin.Default()
+
+	app.Static("/static", "./static")
+
+	app.LoadHTMLGlob("templates/*")
+
+	app.GET("/", func(c *gin.Context) {
+		c.HTML(200, "home.html", gin.H{})
+	})
+
+	app.Run(":8080")
 }
